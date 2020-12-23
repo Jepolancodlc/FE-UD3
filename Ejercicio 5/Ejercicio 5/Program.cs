b3f0d84051d99570d7540ef6bdc42752b0ac3623
Jepolancodlc@gmail.com <Jepolancodlc@gmail.com>
2020-12-23 15:27:46 +0100

    #

---
+++ Ejercicio 5/Ejercicio 5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ejercicio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A= 1, B = 2, C = 3, D = 4;
+			int B2 = B;
+            Console.WriteLine("------------Valores iniciales");
+            Console.WriteLine("Valor de A = "+A);
+            Console.WriteLine("Valor de B = "+B);
+            Console.WriteLine("Valor de C = "+C);
+            Console.WriteLine("Valor de D = "+D);
+            
+            B = C;C = A;
+            A = D; D = B2;
+
+            Console.WriteLine("------------Valores finales");
+            Console.WriteLine("Valor de A = "+A);
+            Console.WriteLine("Valor de B = "+B);
+            Console.WriteLine("Valor de C = "+C);
+            Console.WriteLine("Valor de D = "+D);
+
+
+
+
+        }
+    }
+}
