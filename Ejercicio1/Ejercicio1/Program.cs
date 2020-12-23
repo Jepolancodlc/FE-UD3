0ca0e5c4340e0589e3fe0cbb1da92c523c2eb25d
Jepolancodlc@gmail.com <Jepolancodlc@gmail.com>
2020-12-23 14:29:38 +0100

    #1

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //hecho de esta forma mas "compleja", para testear diferentes formas de programar
+
+            int x = 15, n = 10;
+            Console.WriteLine("El total de la suma es: {0}", x+n);
+            Console.WriteLine("El total de la rests es: {0}", x - n);
+            Console.WriteLine("El total de la multiplicacion es: {0}", x * n);
+            Console.WriteLine("El total de la division es: {0}", x / n);
+            Console.WriteLine("El total del modulo es: {0}", x % n);
+        }
+    }
+}
