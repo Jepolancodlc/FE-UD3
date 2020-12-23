f102a3690dd74cc3b0504f542339b02eaddb2d70
Jepolancodlc@gmail.com <Jepolancodlc@gmail.com>
2020-12-23 14:30:14 +0100

    #

---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=10;
+            double a=3.86;
+            char c='c';
+
+            //hecho de esta forma mas "compleja", para testear diferentes formas de programar
+            Console.WriteLine("Variable N={0}, Variable A={1} Variable C={2}",n,a,c);
+            Console.WriteLine("La suma de N+A es = {0}", n + a); 
+            Console.WriteLine("La diferencia de N+A es = {0}", n - a);
+            Console.WriteLine("Valor numerico del caracter {0} = {1}",c,char.GetNumericValue(c));
+        }
+    }
+}
